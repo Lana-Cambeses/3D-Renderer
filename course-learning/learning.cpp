8bba03a7d4b6e1578e669bec9f5f00edb0676798
Lana-Cambeses <lanacambeses@gmail.com>
2025-06-11 12:20:01 -0700

    videos 8, 9, 10 and 11

--- course-learning/learning.cpp
+++ course-learning/learning.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 void Log(const char* message) {
     std::cout << message << std::endl;
@@ -6,5 +7,19 @@ void Log(const char* message) {
 
 int main() {
     Log("Hello World");
+
+    //POINTERS
+    int var = 8;
+    int* ptr = &var; //Gets the value at that memory address, the memory address of var
+    *ptr = 10; //Sets the memory address to int ten (int bacause we declared ptr as an int size)
+    std::cout << var << std::endl;
+
+    char* buffer = new char[8]; //Im asking the computer to alocate eight bytes of memory for this pointer (char is one, rt?)
+    memset(buffer, 0, 8); //This function takes the pointer as a parameter, what are we filling the memory with as the second, and how much as the third, and sets the memory address with that info
+    
+    char** ptr = &buffer;
+    
+    delete[] buffer; 
+
     std::cin.get();
 }
